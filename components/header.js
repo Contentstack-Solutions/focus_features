7ed8049ad49c6b62787a8500ae6d476908657467
eric-crump <121046143+eric-crump@users.noreply.github.com>
2025-02-16 14:41:28 -0500

    header and footer

--- components/header.js
+++ components/header.js
@@ -1,55 +1,61 @@
+import { useEffect, useState } from "react";
+import Stack, { onEntryChange, cslp } from "@/lib/cstack";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Header(){
-    return(
+export default function Header() {
+    const [entry, setEntry] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getContent = async () => {
+        const entry = await Stack.getElementByTypeWtihRefs("header", "en-us", [
+            'menu.page',
+        ]);
+        console.log("header", entry[0][0]);
+        setEntry(entry[0][0]);
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        onEntryChange(getContent);
+    }, []);
+
+    if (isLoading) return;
+
+    return (
         <div className="sticky top-0 border-b border-black py-[11px] z-50 bg-[#f5f5f2]">
             <div className="flex max-w-[720px] md:max-w-[960px] lg:max-w-[1400px] xl:max-w-8xl mx-auto px-2 justify-between font-oswald items-center text-[20px] xl:text-[24px] font-light">
-                <Image 
-                    className="w-[112px] h-[40px] xl:w-[154px] xl:h-[55px]"
-                    src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blt8607e36bb2c6ecf8/67b09a03a91adf2938673348/logo.png" 
-                    width={154}
-                    height={55}
-                    alt="" 
-                />
-
-                <Link 
-                    href={'#'}
-                    className="header-link"
+                <Link
+                    href="/"
                 >
-                    FILMS
-                </Link>
-                <Link 
-                    href={'#'}
-                    className="header-link"
-                >
-                    FOCUS INSIDER
-                </Link>
-                <Link 
-                    href={'#'}
-                    className="header-link"
-                >
-                    VIDEOS
-                </Link>
-                <Link 
-                    href={'#'}
-                    className="header-link"
-                >
-                    NEWS
-                </Link>
-                <Link 
-                    href={'#'}
-                    className="header-link flex items-center"
-                >
-                    <p>SHOP</p>
                     <Image
-                        className="ml-5 w-[24px] h-[24px]"
-                        src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blta73ffbb85e5ae6f7/67b0a68ed91e1b4448009503/cart.png"
-                        height={24}
-                        width={24}
+                        className="w-[112px] h-[40px] xl:w-[154px] xl:h-[55px]"
+                        src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blt8607e36bb2c6ecf8/67b09a03a91adf2938673348/logo.png"
+                        width={154}
+                        height={55}
                         alt=""
                     />
                 </Link>
+                
+
+                {entry?.menu?.map((item, index) => (
+                    <Link
+                        key={index}
+                        href={item?.page?.length > 0 ? item.page[0].url : "#"}
+                        className="header-link uppercase flex items-center"
+                    >
+                        {item.text}
+                        {item.icon &&
+                            <Image
+                                className="ml-5 w-[24px] h-[24px]"
+                                src={item.icon?.url}
+                                height={24}
+                                width={24}
+                                alt=""
+                            />
+                        }
+                    </Link>
+                ))}
             </div>
         </div>
     )
--- components/productBanner.js
+++ components/productBanner.js
@@ -11,26 +11,6 @@ export default function ProductBanner({ content }) {
                         <p className="font-work font-medium mt-3">{item.price}</p>
                     </div>
                 ))}
-                {/* <div>
-                    <img src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/bltbbb3d7e0446f9f5e/67b1df9bd1508763ed528bc6/Nosferatu_Focus_Features_Logo_Hoodie_f52156fbbd.png" />
-                    <p className="font-oswald text-[24px] lg:text-[32px] mt-2 tracking-tight">Focus Features Nosferatu Logo Hoodie</p>
-                    <p className="font-work font-medium mt-3">FROM: $46.95</p>
-                </div>
-                <div>
-                    <img src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blt1fed54949f5a8a84/67b1df9b7b43d037247f3ad5/For_Site_Classic_Logo_Tote_f4d0c2c4d4.jpeg" />
-                    <p className="font-oswald text-[24px] lg:text-[32px] mt-2 tracking-tight">Focus Features Logo Tote Bag</p>
-                    <p className="font-work font-medium mt-3">FROM: $32.95</p>
-                </div>
-                <div>
-                    <img src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blt18eec337cefbd46a/67b1df9b480898ab95cb9123/focus_sweatshirt_8e626e8d4f.jpeg" />
-                    <p className="font-oswald text-[24px] lg:text-[32px] mt-2 tracking-tight">Focus Features Logo Crewneck</p>
-                    <p className="font-work font-medium mt-3">FROM: $39.95</p>
-                </div>
-                <div>
-                    <img src="https://images.contentstack.io/v3/assets/blt942b6d9738065ec9/blt25088972607e3faf/67b1df9bc9c106f9624389e7/Nos_Focus_Feaures_Horror_Logo_8250da0f7e.png" />
-                    <p className="font-oswald text-[24px] lg:text-[32px] mt-2 tracking-tight">Focus Features Nosferatu Logo Hat</p>
-                    <p className="font-work font-medium mt-3">FROM: $24.95</p>
-                </div> */}
             </div>
             <a 
                 href={content?.page?.length > 0 ? content.page[0].url : "#"}
